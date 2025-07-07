4f34f16ad553091382a657b83771237a75398c00
huijuan_hu <huijuan_hu@intsig.net>
2025-07-07 11:54:21 +0800

    [wn]修改

---
+++ index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>My Vue App</h1>
+    <p>{{ message }}</p>
+    <button @click="changeMessage">Change Message</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello, Vue!'
+    }
+  },
+  methods: {
+    changeMessage() {
+      
+  }
+}
+}
+</script>
